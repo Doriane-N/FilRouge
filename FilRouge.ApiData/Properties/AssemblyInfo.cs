424e5079546f595662b9262c01a26d19389ef76e
Doriane N <narbe.do@gmail.com>
2021-06-09 14:04:43 +0200

    remove authentification

--- FilRouge.ApiData/Properties/AssemblyInfo.cs
+++ FilRouge.ApiData/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de 
 // l’ensemble d’attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("FilRouge.Web")]
+[assembly: AssemblyTitle("FilRouge.ApiData")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FilRouge.Web")]
+[assembly: AssemblyProduct("FilRouge.ApiData")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Le GUID suivant est pour l’ID de la typelib si ce projet est exposé à COM
-[assembly: Guid("f7e7bead-3bbc-4e4a-a98a-707871de4bee")]
+[assembly: Guid("4fc8f954-f2db-4a46-b8b1-a6f2dab60804")]
 
 // Les informations de version pour un assembly se composent des quatre valeurs suivantes :
 //
